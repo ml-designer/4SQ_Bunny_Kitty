86c82c619187d67b65e6f542a6c29e44396883d6
ml-designer <60709608+ml-designer@users.noreply.github.com>
2020-02-10 17:40:29 -0500

    Add files via upload

---
+++ Assets/DeactivationTrigger.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeactivationTrigger : MonoBehaviour
+{
+    //Place on game object being shot (ie Food)
+    [SerializeField] private GameObject displayed;
+
+    //Active before collision
+    void Start()
+    {
+        displayed.SetActive(true);
+    }
+
+    //When food collides with another object tagged "Person", the food is set false
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Person"))
+        {
+            displayed.SetActive(false);
+
+        }
+    }
+}
---
+++ Assets/ShootFood.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootFood : MonoBehaviour
+{
+    //Projectile is food
+    public GameObject projectile;
+    //Camera must be attached to player
+    public Camera cam;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Left mouse button pressed to activate
+        if (Input.GetButtonDown("Fire1"))
+        {
+            //projectile is set active after destruction from DeactivationTrigger.cs
+            projectile.SetActive(true);
+            //Distance from player object (in this case it is shooting from in front of player camera)
+            projectile.transform.position = transform.position + cam.transform.forward * 2;
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+
+            //Speed of food is changed here
+            rb.velocity = cam.transform.forward * 10;
+
+
+        }
+    }
+
+
+}
